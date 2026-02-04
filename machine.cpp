361ea3b80159270e083eff3ee221affbe85f1b42
shenso19 <shenso19@vols.utk.edu>
2026-02-04 11:44:26 -0600

    Update machine.cpp

--- machine.cpp
+++ machine.cpp
@@ -1,41 +1,46 @@
 #include <iostream>
+#include <cstddef>
 #include <cstdint>
 using namespace std;
 
 class Machine {
-private:
-    static constexpr size_t MEMORY_SIZE = 1048576; // 1 MiB
-    uint32_t regs[32]; // array of 32 registers, 32 bits each
-    uint8_t memory[MEMORY_SIZE]; // RAM
-    uint32_t pc; // program counter
-
-    /* Below is a function that will ensure that when register 0 is read that the returned value will always be zero,
-    and when any other register is read that it will return the stored value of that register. */
-    uint32_t read_reg(uint32_t index) {
-        if (index == 0) {
+    private:
+    static constexpr size_t MEMORY_SIZE = 1048576; // 1MiB
+    uint32_t regs[32]; // Array of 32 registers, 32 bits each
+    uint8_t memory[MEMORY_SIZE]; // RAM (Array of 1048576 8 bit values)
+    uint32_t pc; // Program Counter
+    
+    /* Below is a function that will ensure that when register 0 is read the returned value will always be zero. if it is any other register, the function will return the value of that register. */
+    public:
+    uint32_t read_reg(int i) {
+        if (i == 0) {
             return 0;
         } else {
-            return regs[index];
+            return regs[i];
         }
-        }
-
-
-    /* Below is a function that will ensure that when a user attempt to write to register 0 that nothing happens. When a user attempt to write to any other register, the value of
-    the register changes to the input value. */
-    void write_reg(uint32_t index, uint32_t value) {
-        if (index == 0) {
+    }
+    
+    /* Below is a function that will ensure that if there is an attempt to write to register 0 (regs[0]), nothing will happen. When any other register is written to, the new value of the register will be the written value */
+    
+    void write_reg(uint32_t i, uint32_t value) {
+        if (i == 0) {
             return;
-        } else {
-            regs[index] = value;
-
+        }else {
+            regs[i] = value;
         }
-        }
-public:
+    }
+    
+    /* Below is the reset function that sets every value of regs to 0, specifically forces regs 0 to 0, and sets the pc to 0. with public access */
+    
+   
     void reset() {
-        for (uint32_t index = 0; index <= 32; index++) {
-            regs[index] = 0;
+        for (int i = 0; i < 32; i++) { 
+            regs[i] = 0;
+        }
         regs[0] = 0;
         pc = 0;
-        }
+        regs[2] = MEMORY_SIZE;
+    }
+    
 }
-    };
+
