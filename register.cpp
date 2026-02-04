8d7f2aad9c147d974f1d5ee395997d06b65717ad
shenso19 <shenso19@vols.utk.edu>
2026-02-04 11:59:23 -0600

    Update register.cpp

--- register.cpp
+++ register.cpp
@@ -1,1 +1,13 @@
-Placeholder
+class Register {
+    public:
+        Register();
+        
+        uint32_t read(uint8_t reg) const;
+        void write(uint8_t reg, uint32_t value);
+        void reset();
+        void print();
+        
+    private:
+        uint32_t regs[32]; //Array of 32 registers, 32 bits each
+}
+
